a95afdb442a9dcdbe95a6eb362d7a0550a0bada8
eminguyen <emilyhuongnguyen@gmail.com>
2019-03-30 19:55:38 -0700

    Add working crime avoidance

--- public/index.js
+++ public/index.js
@@ -1,3 +1,43 @@
+L.mapquest.key = '7hLeoxAL6EeSLs9z6TDaL6Tmw84yhG1U';
+
+placeSearch({
+  key: '7hLeoxAL6EeSLs9z6TDaL6Tmw84yhG1U',
+  container: document.querySelector('#origin')
+});
+
+placeSearch({
+  key: '7hLeoxAL6EeSLs9z6TDaL6Tmw84yhG1U',
+  container: document.querySelector('#destination')
+});
+
+
+
+// 'map' refers to a <div> element with the ID map
+const map = L.mapquest.map('mapid', {
+ center: [34.0522, -118.2437],
+ layers: L.mapquest.tileLayer('map'),
+ zoom: 13
+});
+
+let directionsLayer;
+
+let directions = L.mapquest.directions();
+
+function routeCallback(error, response) {
+  if(typeof directionsLayer !== 'undefined') {
+    map.removeLayer(directionsLayer);
+  }
+  directionsLayer = L.mapquest.directionsLayer({
+    directionsResponse: response,
+    routeRibbon: {
+      color: "#3cbcbb",
+      opacity: 1.0,
+      showTraffic: false
+    }
+  }).addTo(map);
+  return map;
+}
+
 socket = io();
 
 socket.on('connect', function() {
@@ -23,5 +63,6 @@ $('#routeForm').submit(function(){
   });
 
 socket.on('session', function(data) {
-  console.log(data);
+  console.log(data.sessionId);
+  directions.route(data, routeCallback);
 })
